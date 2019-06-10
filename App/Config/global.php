0e4ea9ee61966ae04cb85fcde85d9ed5b4971c22
Rey Jhon A. Baquirin <jhaybaquirin@yahoo.com>
2019-06-10 17:00:03 +0800

    Update global variables

--- App/Config/global.php
+++ App/Config/global.php
@@ -2,6 +2,8 @@
 /**
  * Application Configuration Settings
  */
+define('APP_NAME','SIMPLE-PHP',true);
+define('APP_DESCRIPTION','The "Simple-PHP" Framework',true);
 define('SHOW_ERRORS', true);
 
 /**
