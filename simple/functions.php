11c42da6eb22c283b102830c2b642de47d1771be
Rey Jhon A. Baquirin <jhaybaquirin@yahoo.com>
2019-06-11 19:20:23 +0800

    add function files for future use

---
+++ simple/functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Core Functions 
+ */
+namespace Simple;
+
+    if (!function_exists(__NAMESPACE__ . '\example'))
+    {
+        function example()
+        {
+            /**
+             * to start using this function in your class, include this line outside your class:
+             *  use function Simple\example;
+             */
+        }
+    } 
