b0181bb0e0b2b96a584016b24e85f0c8774b5176
Rey Jhon <reyjhonbaquirin@yahoo.com>
2019-09-11 16:15:14 +0800

    added dbengine on config

--- app/Config/global.php
+++ app/Config/global.php
@@ -1,1 +1,37 @@
-<?php/** * Application Configuration Settings */define('APP_NAME','Simply PHP',true);define('APP_DESCRIPTION','The "Simply-PHP" Framework',true);define('BASEURL','',true);define('APP_KEY', '');/** * Error handling behaviour * Set to false in production */define('SHOW_ERRORS', true);/** * Options:  *  simply - use the default error handling template. *  whoops - use "filp/whoops" error handling library. *  **IF you set whoops as default ERROR_HANDLER you need to install it. *    run: composer require filp/whoops */define('ERROR_HANDLER','simply', true);/** * Template Engine configuration */define('CACHE_VIEWS', false , true);/** * Database configuration settings */define('DBSERVER', 'localhost',true);define('DBUSER', 'root', true);define('DBPASS', '', true);define('DBNAME','simply', true);
+<?php
+/**
+ * Application Configuration Settings
+ */
+define('APP_NAME','Simply PHP',true);
+define('APP_DESCRIPTION','The "Simply-PHP" Framework',true);
+define('BASEURL','',true);
+define('APP_KEY', '');
+
+/**
+ * Error handling behaviour
+ * Set to false in production
+ */
+define('SHOW_ERRORS', true);
+
+/**
+ * Options: 
+ *  simply - use the default error handling template.
+ *  whoops - use "filp/whoops" error handling library.
+ *  **IF you set whoops as default ERROR_HANDLER you need to install it.
+ *    run: composer require filp/whoops
+ */
+define('ERROR_HANDLER','simply', true);
+
+/**
+ * Template Engine configuration
+ */
+define('CACHE_VIEWS', false , true);
+
+/**
+ * Database configuration settings
+ */
+define('DBENGINE','mysql');
+define('DBSERVER', 'localhost',true);
+define('DBUSER', 'root', true);
+define('DBPASS', '', true);
+define('DBNAME','simply', true);
