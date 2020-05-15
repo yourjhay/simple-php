2814877bfad3931cad482b80365521825926936f
R-Jhay <reyjhonbaquirin@yahoo.com>
2020-05-15 14:09:13 +0800

    Added documentation link on top

--- app/Routes.php
+++ app/Routes.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Application Routes
+ * docs: https://simply-docs.herokuapp.com/documentation/v1/routing
+ */
 use Simple\Routing\Router;
 
 Router::set('',['controller' => 'home', 'action' => 'index']);
