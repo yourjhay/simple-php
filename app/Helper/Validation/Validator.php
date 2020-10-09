3dba9715551f307e83cc3eb63504c24b5d0fd79f
Rey Jhon B <reyjhonbaquirin@yahoo.com>
2020-10-09 09:56:36 +0800

    update unique validator

--- app/Helper/Validation/Validator.php
+++ app/Helper/Validation/Validator.php
@@ -1,26 +1,34 @@
 <?php
+
 namespace App\Helper\Validation;
 
 Use Simple\Model;
 
 class Validator extends BaseValidator
 {
+    /**
+     * Use to check wether the given field and value exist in the database
+     * @param $field
+     * @param $input
+     * @param array $param
+     * @return array|void
+     */
     public function validate_unique(
         $field,
         $input,
-        $param = null
+        $param = []
     ) {
         self::set_error_messages(array(
-            "validate_unique"     => "{field} is already Taken.",
+            "unique"    =>  "{field} is already Taken.",
         ));
-        $call = Model::unique_checker($param,$field,$input[$field]);
+        $call = Model::unique_checker($param, $field, $input[$field]);
         if(!$call) {
             return;
         }
         return array(
             'field' => $field,
             'value' => $input[$field],
-            'rule' => __FUNCTION__,
+            'rule'  => __FUNCTION__,
             'param' => $param,
         );
 	}
